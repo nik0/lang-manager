0da47990637e6af0b7031ffa22117eb743787fa0
Thebigboss33 <Thebigboss33@users.noreply.github.com>
2021-02-21 05:21:21 +0100

    All of the project

---
+++ index.js
@@ -0,0 +1,32 @@
+//Electron initialisation
+const { app, BrowserWindow, ipcMain } = require("electron")
+const path = require('path')
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        title: "Lang manager",
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    mainWindow.loadURL(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+    createWindow() //Create the window
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit() //Close the window
+})
+
+//Lang manager
